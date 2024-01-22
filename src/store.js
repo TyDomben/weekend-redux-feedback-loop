b21a30ccd2accfdad4e46a692de691d4354c3fb9
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2024-01-21 20:19:58 -0600

    added test fields

--- src/store.js
+++ src/store.js
@@ -1,31 +1,30 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import logger from 'redux-logger';
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
 
 // Reducer for managing feedback
 const feedback = (state = {}, action) => {
-    switch (action.type) {
-        case 'SET_FEELING':
-            return { ...state, feeling: action.payload };
-        case 'SET_UNDERSTANDING':
-            return { ...state, understanding: action.payload };
-        case 'SET_SUPPORT':
-            return { ...state, support: action.payload };
-        case 'SET_COMMENTS':
-            return { ...state, comments: action.payload };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "SET_FEELING":
+      return { ...state, feeling: action.payload };
+    case "SET_UNDERSTANDING":
+      return { ...state, understanding: action.payload };
+    case "SET_SUPPORT":
+      return { ...state, support: action.payload };
+    case "SET_COMMENTS":
+      return { ...state, comments: action.payload };
+    case "RESET_FEEDBACK":
+      return {}; // Reset to initial state
+    default:
+      return state;
+  }
 };
 
 // combine reducers
 const rootReducer = combineReducers({
-    feedback,
+  feedback,
 });
 
 // Create the store
-const store = createStore(
-    rootReducer,
-    applyMiddleware(logger)
-);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
